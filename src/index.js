8ee9bfea7b2c934c53ba421c7fb8ac9789518f8a
gmc314 <grant.cheng1@gmail.com>
2024-02-07 15:44:50 -0500

    add robots

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './components/Card';
+import { robots } from './components/robots'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-    <Card /> <Card /> <Card />
+    <Card name={ robots[0].name } email={ robots[0].email } id={ robots[0].id }/> 
+    <Card name={ robots[1].name } email={ robots[1].email } id={ robots[1].id }/> 
+    <Card name={ robots[2].name } email={ robots[2].email } id={ robots[2].id }/>
     </div>
   </React.StrictMode>
 );
