97482337915c231d8255f901d09d82ee272286a3
gmc314 <grant.cheng1@gmail.com>
2024-02-07 15:48:25 -0500

    indent

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-    <Card name={ robots[0].name } email={ robots[0].email } id={ robots[0].id }/> 
-    <Card name={ robots[1].name } email={ robots[1].email } id={ robots[1].id }/> 
-    <Card name={ robots[2].name } email={ robots[2].email } id={ robots[2].id }/>
+      <Card name={ robots[0].name } email={ robots[0].email } id={ robots[0].id }/> 
+      <Card name={ robots[1].name } email={ robots[1].email } id={ robots[1].id }/> 
+      <Card name={ robots[2].name } email={ robots[2].email } id={ robots[2].id }/>
     </div>
   </React.StrictMode>
 );
