517f30850fbbef26463bcb15d00bd86bd26f2497
gmc314 <grant.cheng1@gmail.com>
2024-02-14 15:16:37 -0500

    try implementing search box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
 import { robots } from "./components/robots";
+ 
+class App extends Component  {
+  constructor() {
+  super();
+    this.state  = {
+      robots: robots,
+      searchfield: ""
+    };
+    this.onChange = this.onSearchChange.bind(this);
+  }
+  onSearchChange (e) {
+    console.log(e.target.value)
+  }
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={ robots }/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox SearchChange={ this.onChange } />
+        <CardList robots={ this.state.robots }/>
+      </div>
+    );
+  }
 }
-
+/*<input
+          className="pa3 ba b--blue bg-lightest-blue"
+          type="search" 
+          placeholder="Search Robots"
+          onChange={this.onSearchChange}
+          >
+        </input>
+        */
 export default App;
