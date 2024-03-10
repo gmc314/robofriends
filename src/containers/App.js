5d7be98d98b44b256380246d4a833697b009082e
gmc314 <grant.cheng1@gmail.com>
2024-03-09 19:45:08 -0500

    pre-build

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,24 +24,22 @@ class App extends Component  {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(bot => {
-      return bot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    })
-
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    }
-    else {
-      return (
-        <div className="tc">
-          <h1 className="f1">Robots</h1>
-          <SearchBox searchChange={ this.onSearchChange } />
-          <Scroll>
-            <CardList robots={ filteredRobots }/>
-          </Scroll>             
-        </div>
-      );
-    }
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(bot => {
+      return bot.name.toLowerCase().includes(searchfield.toLowerCase())
+      }
+    )
+    return !robots.length ? 
+    <h1>Loading</h1>  
+    : (
+      <div className="tc">
+        <h1 className="f1">Robots</h1>
+        <SearchBox searchChange={ this.onSearchChange } />
+        <Scroll>
+          <CardList robots={ filteredRobots }/>
+        </Scroll>             
+      </div>
+    );
   }
 }
 
