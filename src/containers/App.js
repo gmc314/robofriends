ec60fd71079fca08080e5a6a5fb097a9f7d37dd3
gmc314 <grant.cheng1@gmail.com>
2024-03-09 19:33:36 -0500

    implement scroll

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./components/CardList";
-import SearchBox from "./components/SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import './App.css'; 
 
 class App extends Component  {
@@ -35,7 +36,9 @@ class App extends Component  {
         <div className="tc">
           <h1 className="f1">Robots</h1>
           <SearchBox searchChange={ this.onSearchChange } />
-          <CardList robots={ filteredRobots }/>  
+          <Scroll>
+            <CardList robots={ filteredRobots }/>
+          </Scroll>             
         </div>
       );
     }
