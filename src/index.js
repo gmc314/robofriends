4954b4531d7220a25292db2dd5c0e177ea4bf9c1
gmc314 <grant.cheng1@gmail.com>
2024-09-02 15:11:30 -0400

    update createRoot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
+import { Provider, connect } from 'react-redux';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App/>
